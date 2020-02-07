0b43e2010f883224ad878ee302f5bd0b47191bf9
kjaa34 <kjaa34@gmail.com>
2020-02-08 03:43:03 +0900

    게시글 수정/삭제 기능 완성

--- src/main/java/com/skykimpro/book/springboot/service/PostsService.java
+++ src/main/java/com/skykimpro/book/springboot/service/PostsService.java
@@ -2,14 +2,17 @@ package com.skykimpro.book.springboot.service;
 
 import com.skykimpro.book.springboot.domain.posts.Posts;
 import com.skykimpro.book.springboot.domain.posts.PostsRepository;
+import com.skykimpro.book.springboot.web.dto.PostsListResponseDto;
 import com.skykimpro.book.springboot.web.dto.PostsResponseDto;
 import com.skykimpro.book.springboot.web.dto.PostsSaveRequestDto;
 import com.skykimpro.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 @RequiredArgsConstructor
 @Service
@@ -33,6 +36,20 @@ public class PostsService {
         return id;
     }
 
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
+        postsRepository.delete(posts);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
 
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id)
